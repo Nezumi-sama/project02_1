1ea963507c6508d0ed683237a941afffc9fa95d8
Nezumi-sama <kryska007@yandex.ru>
2022-02-01 23:54:00 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //объявлем переменные и вводим данные
+        int price = 97;
+        int calculate;
+
+        //рассчет
+        calculate = price / 20;
+        System.out.println("Начислено бесплатных миль: " + calculate);
+    }
+}
